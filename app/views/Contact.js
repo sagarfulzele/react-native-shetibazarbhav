d10572e3df3f614e1f7d642cb9b5ecc4e13487f8
Fulzele <sagarchandrakant.fulzele@monster.com>
2018-07-08 12:37:19 +0800

    Signed-off-by: Sagar

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -13,10 +13,10 @@ export class Contact extends React.Component{
         <Text style={{flex:8}}>conact form</Text>
         <Text style={{flex:6}}>conact form</Text>
    </View>  
-        
+            
     } 
 }
-
+       
 
 const styles = StyleSheet.create({
     container: { flex: 1, paddingTop:30 },
--- app/views/Home.js
+++ app/views/Home.js
@@ -18,7 +18,7 @@ export class Home extends React.Component{
     }
 
 }
-
+    
 
 const styles = StyleSheet.create({
     container: { flex: 1, paddingTop:30 },
