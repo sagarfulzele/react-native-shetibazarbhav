6dd7fbd3cff61bb7762dc6b356829fafd8d82b0e
Fulzele <sagarchandrakant.fulzele@monster.com>
2018-07-08 12:44:01 +0800

    Signed-off-by: sagar

--- App.test.js
+++ App.test.js
@@ -6,4 +6,4 @@ import renderer from 'react-test-renderer';
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
-});
+}); 
